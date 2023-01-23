0e34294f55ed6589ce657139fc7e55fb619d6bca
cosycolor <handyoon87@gmail.com>
2023-01-23 19:47:36 +0900

    refactoring AppConfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,10 +8,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
+
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderServiceImpl orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+    }
+
+    private static FixedDiscountPolicy getDiscountPolicy() {
+        return new FixedDiscountPolicy();
     }
 
 }
