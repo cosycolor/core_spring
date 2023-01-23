5df7e0b673a4c4a6bbc0f2d94c96300108907613
cosycolor <handyoon87@gmail.com>
2023-01-23 21:01:13 +0900

    spring conversion

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,26 +1,36 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(getMemberRepository());
     }
 
+    @Bean
     private static MemoryMemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderServiceImpl orderService(){
         return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 
-    private static FixedDiscountPolicy getDiscountPolicy() {
-        return new FixedDiscountPolicy();
+    @Bean
+    private static DiscountPolicy getDiscountPolicy() {
+        //return new FixedDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
