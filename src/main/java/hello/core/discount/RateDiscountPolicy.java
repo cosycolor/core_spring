ad49d9db1a7ac5a15251f6915d8474701389657d
cosycolor <handyoon87@gmail.com>
2023-01-23 18:17:26 +0900

    discountRatePolicy add

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private final int discountRate = 10;
+
+
+    @Override
+    public int discountPolicy(Member member, int price) {
+        if(member.getGrade().equals(Grade.VIP)){
+            return price * discountRate/100;
+        }else{
+            return 0;
+        }
+
+    }
+}
