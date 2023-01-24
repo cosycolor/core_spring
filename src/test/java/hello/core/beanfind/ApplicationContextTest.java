b89c2e19ce3a70b6e5b07bb5da185bea7315a2d8
cosycolor <handyoon87@gmail.com>
2023-01-24 17:02:22 +0900

    singleton and beanfind

---
+++ src/test/java/hello/core/beanfind/ApplicationContextTest.java
@@ -0,0 +1,22 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("스프링 빈 확인")
+    public void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean = " + bean);
+
+        }
+    }
+}
