a4d04f740e8cc855ffe1f037e64744428440a680
cosycolor <handyoon87@gmail.com>
2023-01-25 21:10:31 +0900

    autowiredTest & add lombok

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     //배우가 직접 캐스팅도 하는 격, 외부에서 주입해주어 캐스팅의 역할을 분리하자
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,7 +1,9 @@
 package hello.core.member;
 
-import java.util.HashMap;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+@Component
 public class MemoryMemberRepository implements MemberRepository{
     private static HashMap<Long, Member> store = new HashMap<>();
     @Override
